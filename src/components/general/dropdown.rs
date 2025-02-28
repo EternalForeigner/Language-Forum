c2d40e279b07f3da7cba24fc9ce97789d9e1453e
Andrew <eternalforeigner@gmx.com>
2025-02-28 16:33:21 +0000

    added TODO for dropdown

--- src/components/general/dropdown.rs
+++ src/components/general/dropdown.rs
@@ -2,6 +2,8 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn Dropdown(is_open: bool, menu: Element) -> Element {
+
+    // TODO: https://github.com/samtay/birdtalk/blob/5a8b1ec0263b81619f591e00afa1edc99ec8cf74/app/src/ui/components/modal.rs
     rsx! {
         if is_open {
             div { class: "absolute right-0 overflow-auto transform transition-all duration-300 ease-in-out -translate-y-2 translate-y-0 mt-2",
