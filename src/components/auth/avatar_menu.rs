7e637a3e189f2d39ed5992118d00210326671500
Andrew <eternalforeigner@gmx.com>
2025-03-02 09:32:05 +0000

    avatar image subscribes to user

--- src/components/auth/avatar_menu.rs
+++ src/components/auth/avatar_menu.rs
@@ -11,7 +11,7 @@ use crate::Route;
 const MENU_ITEM_CLASSES: &str = "cursor-pointer block transition hover:bg-zinc-950 py-4 px-8";
 
 #[component]
-pub fn AvatarMenu(user: User) -> Element {
+pub fn AvatarMenu(user: ReadOnlySignal<User>) -> Element {
     let nav = use_navigator();
     let mut is_dropdown_open = use_signal(|| false);
 
@@ -27,7 +27,7 @@ pub fn AvatarMenu(user: User) -> Element {
                 onclick: move |_| is_dropdown_open.set(!is_dropdown_open()),
                 ImageOrFallback {
                     image_class: "w-10 h-10 m-auto",
-                    image_url: get_avatar_url(user.id),
+                    image_url: get_avatar_url(user().id),
                     fallback: rsx! {
                         Icon { class: "w-10 h-10 m-auto", icon: FaUser }
                     },
