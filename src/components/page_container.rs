b67ec090338a4e00e2800997a1a7f4b0bbb8c385
Andrew <eternalforeigner@gmx.com>
2025-03-10 11:26:13 +0000

    adjusted page bg colors

--- src/components/page_container.rs
+++ src/components/page_container.rs
@@ -5,6 +5,8 @@ pub fn PageContainer(children: Element, extra_classes: Option<String>) -> Elemen
     let extra_class_string = extra_classes.unwrap_or("".into());
 
     rsx! {
-        div { class: "container mx-auto px-8 py-4 {extra_class_string}", {children} }
+        div {class: "h-min-full bg-white dark:bg-slate-950",
+            div { class: "container mx-auto px-8 py-4 {extra_class_string}", {children} }
+        }
     }
 }
