cfb5a1433d57d02a15ee4839b6bd012c47b1a75c
Andrew <eternalforeigner@gmx.com>
2025-03-10 14:33:03 +0000

    reformat class consts

--- src/components/class_consts.rs
+++ src/components/class_consts.rs
@@ -1,7 +1,12 @@
 pub const LINK_CLASSES: &str = "cursor-pointer underline hover:no-underline hover:sky-amber-300 transition duration-200 ease-in-out";
+
 pub const LABEL_CLASSES: &str = "text-gray-950 dark:text-gray-50";
-pub const INPUT_CLASSES: &str = 
-"py-4 px-4 border rounded-md focus:outline-none bg-gray-50 dark:bg-gray-300 border-gray-400 text-gray-700 dark:text-gray-700 focus:bg-white dark:focus:bg-gray-200 focus:border-2 focus:border-blue-500";
-pub const BUTTON_CLASSES: &str = 
-"cursor-pointer bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out focus:outline-none focus:shadow-outline";
-pub const SUCCESS_CLASSES: &str = "text-current text-sm mt-4 bg-green-400 rounded border border-green-700 p-4";
+
+pub const INPUT_CLASSES: &str = "py-4 px-4 border rounded-md focus:outline-none bg-gray-50 dark:bg-gray-300 border-gray-400 \
+    text-gray-700 dark:text-gray-700 focus:bg-white dark:focus:bg-gray-200 focus:border-2 focus:border-blue-500";
+
+pub const BUTTON_CLASSES: &str = "cursor-pointer bg-sky-500 hover:bg-sky-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out \
+    focus:outline-none focus:shadow-outline";
+    
+pub const SUCCESS_CLASSES: &str =
+    "text-current text-sm mt-4 bg-green-400 rounded border border-green-700 p-4";
