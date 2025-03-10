9c90708b70ad318d86d3cef4d4b920e9a79432a9
Andrew <eternalforeigner@gmx.com>
2025-03-10 11:56:12 +0000

    categories table adjusted colors

--- src/components/forum/categories_table.rs
+++ src/components/forum/categories_table.rs
@@ -42,7 +42,10 @@ pub fn CategoriesTable(parent_id: Option<i64>) -> Element {
                     ForumTable {
                         extra_classes: "border border-gray-400",
                         head: rsx! {
-                            ForumTableHeader { titles: HEADERS.iter().map(|string| String::from(*string)).collect() }
+                            ForumTableHeader {
+                                extra_classes: "bg-blue-100 dark:bg-slate-800 text-gray-800 dark:text-gray-400",
+                                titles: HEADERS.iter().map(|string| String::from(*string)).collect(),
+                            }
                         },
                         rows: categories
                             .iter()
--- src/components/general/forum_table_header.rs
+++ src/components/general/forum_table_header.rs
@@ -1,15 +1,16 @@
 use dioxus::prelude::*;
 
 #[component]
-pub fn ForumTableHeader(titles: Vec<String>) -> Element {
+pub fn ForumTableHeader(titles: Vec<String>, extra_classes: Option<String>) -> Element {
     rsx! {
         thead {
             tr {
                 for (index , title) in titles.iter().enumerate() {
                     {
                         let classes = format!(
-                            "px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider {}",
+                            "px-6 py-3 text-left text-xs font-medium uppercase tracking-wider {} {}",
                             if index == 0 { "w-auto" } else { "" },
+                            extra_classes.clone().unwrap_or_default(),
                         );
                         rsx! {
                             th { class: classes, {title.clone()} }
