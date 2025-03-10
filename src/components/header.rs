b896328e9eb94962080892c07c2d53e42a1b8654
Andrew <eternalforeigner@gmx.com>
2025-03-10 10:48:59 +0000

    header dark mode

--- src/components/header.rs
+++ src/components/header.rs
@@ -2,7 +2,7 @@ use crate::{components::auth::AvatarMenu, hooks::use_supabase, Route};
 use dioxus::prelude::*;
 
 // const LOGO_CSS: Asset = asset!("/assets/styling/logo.css");
-const LINK_CLASSES: &str = "transition hover:bg-zinc-950";
+const LINK_CLASSES: &str = "transition hover:bg-zinc-300 dark:hover:bg-zinc-700 text-black dark:text-white";
 
 fn get_auth_links() -> Result<VNode, RenderError> {
     rsx! {
@@ -35,14 +35,14 @@ pub fn Header() -> Element {
     rsx! {
         // document::Link { rel: "stylesheet", href: LOGO_CSS }
 
-        div { class: "flex p-0 bg-zinc-900",
+        div { class: "flex p-0 bg-zinc-200 dark:bg-zinc-900",
             Link {
-                class: "{LINK_CLASSES} logo-background-transition flex py-2 px-4",
+                class: "flex py-2 px-4 {LINK_CLASSES} ",
                 to: Route::Home {},
                 div { class: "flex px-4",
                     span { class: "span-svg-icon gradient gradient-svg m-auto" }
                 }
-                div { class: "text-white text-2xl content-center",
+                div { class: "text-2xl content-center",
                     "Language Forum"
                 }
             }
