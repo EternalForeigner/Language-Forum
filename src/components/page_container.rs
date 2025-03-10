d4095875780f045c4ddf8b09d6799230cb96dfdf
Andrew <eternalforeigner@gmx.com>
2025-03-10 15:37:47 +0000

    full screen height pages

--- src/components/page_container.rs
+++ src/components/page_container.rs
@@ -2,10 +2,10 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn PageContainer(children: Element, extra_classes: Option<String>) -> Element {
-    let extra_class_string = extra_classes.unwrap_or("".into());
+    let extra_class_string = extra_classes.unwrap_or_default();
 
     rsx! {
-        div {class: "h-min-full bg-white dark:bg-slate-950",
+        div {class: "flex-1 bg-white dark:bg-slate-950",
             div { class: "container mx-auto px-8 py-4 {extra_class_string}", {children} }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,6 @@ enum Route {
 }
 
 // const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/styling/main.css");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
@@ -56,11 +55,9 @@ fn App() -> Element {
     use_supabase_provider(&env::APP_SUPABASE_URL, &env::APP_SUPABASE_KEY);
 
     rsx! {
-        // Global app resources
         // document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS }
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
 
-        Router::<Route> {}
+        div { class: "min-h-screen flex flex-col", Router::<Route> {} }
     }
 }
