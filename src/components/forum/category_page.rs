9a256c958ea5f9a6b0af43003d7f728a5f084e2f
Andrew <eternalforeigner@gmx.com>
2025-03-11 14:29:05 +0000

    added new thread button

--- src/components/forum/category_page.rs
+++ src/components/forum/category_page.rs
@@ -1,17 +1,19 @@
 use dioxus::prelude::*;
-use supabase_rs::Result;
+use supabase_rs::{Result, Supabase};
 
 use crate::{
     components::{
         forum::{CategoriesTable, ThreadsTable},
         general::ErrorNotice,
+        BUTTON_CLASSES,
     },
     hooks::use_supabase,
     models::Category as CategoryModel,
+    Route,
 };
 
-async fn get_category(id: i64) -> Result<Option<CategoryModel>> {
-    let response = use_supabase()
+async fn get_category(mut supabase: Supabase, id: i64) -> Result<Option<CategoryModel>> {
+    let response = supabase
         .from("categories")
         .await?
         .eq("id", id.to_string())
@@ -25,16 +27,27 @@ async fn get_category(id: i64) -> Result<Option<CategoryModel>> {
 
 #[component]
 pub fn CategoryPage(category_id: ReadOnlySignal<i64>) -> Element {
-    let category = use_resource(move || get_category(category_id())).suspend()?;
+    let supabase = use_supabase();
+    let is_logged_in = supabase.is_logged_in();
+    let category = use_resource(move || get_category(supabase.clone(), category_id())).suspend()?;
 
-    rsx!{
+    rsx! {
         match &*category.read() {
             Ok(category) => {
                 if let Some(category) = category {
                     rsx! {
                         h1 { class: "my-2 text-3xl text-gray-950 dark:text-white", {category.name.clone()} }
                         p { class: "text-sm text-gray-700 dark:text-gray-300", {category.description.clone()} }
-                        div { class: "my-8" }
+                        div { class: "flex my-4 justify-end",
+                            if is_logged_in {
+                                Link {
+                                    to: Route::CreateThread {
+                                        category_id: category.id,
+                                    },
+                                    button { class: BUTTON_CLASSES, "New Thread" }
+                                }
+                            }
+                        }
                         CategoriesTable { parent_id: category_id() }
                         ThreadsTable { category_id: category_id() }
                     }
