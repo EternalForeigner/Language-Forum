8afcf7e04c36ce617df8686be9eae528aac98839
Andrew <eternalforeigner@gmx.com>
2025-03-12 10:45:02 +0000

    format post date time

--- src/components/forum/post.rs
+++ src/components/forum/post.rs
@@ -6,7 +6,7 @@ use crate::{components::forum::PostProfileDisplay, models::PostView};
 pub fn Post(post_view: PostView) -> Element {
     rsx! {
         div { class: "px-2 py-2 min-w-full bg-blue-100 dark:bg-gray-950 text-gray-700 dark:text-gray-300",
-            {post_view.created_at.unwrap_or_default().to_string()}
+            {post_view.created_at.map_or(String::from("Unknown time"), |d| d.format("%B %d, %Y %H:%M").to_string())}
         }
         div { class: "flex min-w-full bg-slate-50 dark:bg-slate-800 text-gray-900 dark:text-gray-100",
             PostProfileDisplay { post_view: post_view.clone() }
